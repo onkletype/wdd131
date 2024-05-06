1d6214abc474a40c1f14320f4a7af2f851cf4e81
onkletype <daeson.grover@gmail.com>
2024-05-05 23:21:41 -0600

    bio website

---
+++ scripts/getDates.js
@@ -0,0 +1,4 @@
+const date = new Date();
+let year = date.getFullYear;
+document.getElementById("currentYear").innerHTML = date.getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
