779cbd25e3d2f1185564a6481e576d2768b6e7d2
onkletype <daeson.grover@gmail.com>
2024-05-18 16:56:52 -0600

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,11 @@ const date = new Date();
 let year = date.getFullYear;
 document.getElementById("currentYear").innerHTML = date.getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
+// declare the varialble for ham menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navMenu');
+// using the ham menu to open an d close
+hamButton.addEventListener('click', ()=>{
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+})
